708d1e41ac94fbc41e38477afb55234b34b65269
alexmndzdev <alejandro.mendez.developer@gmail.com>
2021-03-23 23:28:21 -0600

    Finished cart component | Continue in react-router part of video

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,50 @@
+import { Typography, Container, Button, Grid } from '@material-ui/core'
+import React from 'react'
+import useStyles from './styles.js'
+import CartItem from './CartItem/CartItem'
+
+const Cart = ({ cart }) => {
+	const classes = useStyles()
+
+	const EmptyCart = () => (
+		<Typography variant="subtitle1">No tienes articulos en tu carrito, empieza a agregar algunos!</Typography>
+	)
+
+	const FilledCart = () => (
+		<>
+			<Grid container spacing={3}>
+				{cart.line_items.map((item) => (
+					<Grid item xs={12} sm={4} key={item.key}>
+						<CartItem item={item}/>
+					</Grid>
+				))}
+			</Grid>
+			<div className={classes.cardDetails}>
+				<Typography variant="h4">
+					Subtotal: {cart.subtotal.formatted_with_symbol}
+				</Typography>
+				<div>
+					<Button className="classes.emptyButton" size="large" type="button" variant="contained" color="secondary">
+						Vaciar carrito
+					</Button>
+					<Button className="classes.checkoutButton" size="large" type="button" variant="contained" color="primary">
+						Ir a caja
+					</Button>
+				</div>
+			</div>
+		</>
+	)
+	
+	if (!cart.line_items) return 'Cargando...'
+	const isEmpty = !cart.line_items.length
+
+	return (
+		<Container>
+			<div className={classes.toolbar} />
+			<Typography className={classes.title} variant="h3" gutterBottom>Carrito</Typography>
+			{ isEmpty ? <EmptyCart /> : <FilledCart /> }
+		</Container>
+	)
+}
+
+export default Cart
---
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import useStyles from './styles'
+import { Card, CardMedia, CardContent, CardActions, Typography, Button } from '@material-ui/core'
+
+const CartItem = ({ item }) => {
+	const classes = useStyles()
+	return (
+		<Card>
+			<CardMedia image={item.media.source}  alt={item.name} className={classes.media}/>	
+			<CardContent className={classes.cardContent}>
+				<Typography variant="h4">{item.name}</Typography>
+				<Typography variant="h5">{item.line_total.fromatted_with_symbol}</Typography>
+			</CardContent>
+			<CardActions className={classes.cardActions}>
+				<div className={classes.buttons}>
+					<Button type="button" size="small">-</Button>
+					<Typography>{item.quantity}</Typography>
+					<Button type="button" size="small">+</Button>
+				</div>
+				<Button variant="contained" type="button" color="secondary">Remover</Button>
+			</CardActions>
+		</Card>
+	)
+}
+
+export default CartItem
