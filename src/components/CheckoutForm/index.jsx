f78a34c5a2f684455b247b3a8daeed4ad10d82f1
Alejandro Méndez <alejandro.mendez.developer@gmail.com>
2022-10-10 14:43:25 -0500

    Remove stepper to deploy

--- src/components/CheckoutForm/index.jsx
+++ src/components/CheckoutForm/index.jsx
@@ -1,25 +1,14 @@
-import React, { useState } from 'react'
-import { Stepper, Step, StepLabel, Typography } from '@mui/material'
+import React from 'react'
+//import { Stepper, Step, StepLabel, Typography } from '@mui/material'
 import useStyles from './styles'
 
-const steps = ['Dirección de envio', 'Detalle de Pago']
+//const steps = ['Dirección de envio', 'Detalle de Pago']
 
 const Checkout = () => {
-	const [activeStep, setActiveStep] = useState(0)
 	const classes = useStyles()
 	return (
 		<>
 			<div className={classes.toolbar} />
-			<main className={classes.layout}>
-				<Typography variant="h4" align="center">Checkout</Typography>
-				<Stepper activeStep={0}>
-					{steps.map((step) => (
-						<Step key={step}>
-							<StepLabel>{step}</StepLabel>
-						</Step>
-					))}
-				</Stepper>
-			</main>
 		</>
 	)
 }
