7343b4e0cef62732742e406bb8ff11e4ce577084
amendez <amendez@mikiosko.mx>
2024-03-30 21:08:08 -0600

    Add dependency in useEffect in product component by warning of netlify

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -6,14 +6,13 @@ import Product from './Product'
 import useStyles from './style'
 
 const Products = ({ category, onAddToCart }) => {
-	console.log(category, 'category')
 	const [products, setProducts] = useState([])
 	const fetchProductsByCategory = async (category) => {
 		const { data } = await commerce.products.list({ category_id: category.id })
 		setProducts(data)
 	}
 	
-	useEffect(() => {
+	useEffect((category) => {
 		fetchProductsByCategory(category)
 	}, [])
 
